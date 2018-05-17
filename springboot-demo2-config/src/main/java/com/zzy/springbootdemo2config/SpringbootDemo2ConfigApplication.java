51c884487d1048fc5e90266194b6cb21f720bd9d
AkihaChang <akihachang@163.com>
2018-05-17 17:27:38 +0800

    2018.05.17

---
+++ springboot-demo2-config/src/main/java/com/zzy/springbootdemo2config/SpringbootDemo2ConfigApplication.java
@@ -0,0 +1,12 @@
+package com.zzy.springbootdemo2config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo2ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo2ConfigApplication.class, args);
+    }
+}
