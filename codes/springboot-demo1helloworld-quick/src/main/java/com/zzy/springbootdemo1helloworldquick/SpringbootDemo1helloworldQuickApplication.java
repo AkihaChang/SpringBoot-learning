27151f46c31f2eb8260c0d878db686c1ecf1b88d
AkihaChang <akihachang@163.com>
2018-05-17 17:03:37 +0800

    2018.05.17

---
+++ codes/springboot-demo1helloworld-quick/src/main/java/com/zzy/springbootdemo1helloworldquick/SpringbootDemo1helloworldQuickApplication.java
@@ -0,0 +1,12 @@
+package com.zzy.springbootdemo1helloworldquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo1helloworldQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo1helloworldQuickApplication.class, args);
+    }
+}
---
+++ codes/springboot-demo1helloworld-quick/src/test/java/com/zzy/springbootdemo1helloworldquick/SpringbootDemo1helloworldQuickApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzy.springbootdemo1helloworldquick;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDemo1helloworldQuickApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
