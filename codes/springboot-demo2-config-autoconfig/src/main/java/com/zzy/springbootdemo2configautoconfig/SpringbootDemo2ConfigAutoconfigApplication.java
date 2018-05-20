ec948cff5f1bbdcdc8f461de49ac5720071993bc
AkihaChang <akihachang@163.com>
2018-05-20 16:52:40 +0800

    2018.05.20

---
+++ codes/springboot-demo2-config-autoconfig/src/main/java/com/zzy/springbootdemo2configautoconfig/SpringbootDemo2ConfigAutoconfigApplication.java
@@ -0,0 +1,12 @@
+package com.zzy.springbootdemo2configautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo2ConfigAutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo2ConfigAutoconfigApplication.class, args);
+    }
+}
---
+++ codes/springboot-demo2-config-autoconfig/src/test/java/com/zzy/springbootdemo2configautoconfig/SpringbootDemo2ConfigAutoconfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzy.springbootdemo2configautoconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDemo2ConfigAutoconfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
